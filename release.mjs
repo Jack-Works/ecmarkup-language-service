bbabd48cca74fd065690b904515bd7eb525574cc
Jack Works <jackworks@protonmail.com>
2022-12-11 12:11:50 +0800

    chore: rename files and extension id

---
+++ release.mjs
@@ -0,0 +1,13 @@
+import { spawnSync } from 'child_process'
+import { readFileSync, writeFileSync } from 'fs'
+
+const repoName = 'ecmarkup-language-extension-vscode'
+const publishName = 'ecmarkup'
+const manifest = new URL('./extension-vscode/package.json')
+const file = readFileSync(manifest, 'utf-8')
+
+spawnSync('pnpm run build', { shell: true, stdio: 'inherit' })
+writeFileSync(manifest, file.replace(repoName, publishName))
+spawnSync('pnpm -C extension-vscode run vsce-publish', { shell: true, stdio: 'inherit' })
+writeFileSync(manifest, file)
+spawnSync('npx changeset tag', { shell: true, stdio: 'inherit' })
