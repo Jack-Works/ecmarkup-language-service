af55213dff011e1b1bbdf5d3ce29449fd2281e8a
Jack Works <5390719+Jack-Works@users.noreply.github.com>
2025-09-27 14:28:43 +0800

    fix: ci

--- release.mjs
+++ release.mjs
@@ -8,6 +8,7 @@ const file = readFileSync(manifest, 'utf-8')
 
 spawnSync('yarn build', { shell: true, stdio: 'inherit' })
 writeFileSync(manifest, file.replace(repoName, publishName))
-spawnSync('yarn workspace ecmarkup-language-extension-vscode run vsce-publish --no-dependencies', { shell: true, stdio: 'inherit' })
+spawnSync('yarn --no-immutable --immutable-cache', { shell: true, stdio: 'inherit' })
+spawnSync('yarn run vsce-publish', { shell: true, stdio: 'inherit', cwd: new URL('./extension-vscode/', import.meta.url) })
 writeFileSync(manifest, file)
 spawnSync('yarn changeset tag', { shell: true, stdio: 'inherit' })
